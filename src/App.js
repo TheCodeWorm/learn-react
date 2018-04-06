5ceec735bc6f4cf5fe66c99a91559d80802da0fd
TheCodeWorm <4purposeliving@gmail.com>
2018-04-06 14:37:18 -0700

    button added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleClick = () => {
+    import('./moduleA')
+      .then(({ moduleA }) => {
+      // Use moduleA
+    })
+    .catch(err => {
+      // Handle failure
+    });
+  };
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div> 
+        <button onClick={this.handleClick}>Load</button>
       </div>
     );
   }
