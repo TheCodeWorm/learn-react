88cb267668e80318c8f39b7c0514d277f587002f
TheCodeWorm <4purposeliving@gmail.com>
2018-04-07 19:15:19 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let result = 3;
+
 class App extends Component {
+  result = 7;
   handleClick = () => {
     import('./moduleA')
       .then(({ moduleA }) => {
       document.write("testing testing testing...");
+      document.write(result);
     })
     .catch(err => {
       // Handle failure
@@ -13,10 +17,11 @@ class App extends Component {
   };
 
   render() {
-
     return (
-      <div> 
-        <button onClick={this.handleClick}>Login</button>
+      <div style={{textAlign:'center'}}> 
+        <button onClick={this.handleClick}>7</button>
+        <button onClick={this.handleClick}>8</button>
+        <span> </span>
       </div>
     );
   }
