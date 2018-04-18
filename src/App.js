fa28dc9905ce809a2b5e692f001574fb26f7c998
TheCodeWorm <4purposeliving@gmail.com>
2018-04-18 14:39:04 -0700

    u

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { contacts } from './contacts';
 
-const App = () => {
-	return (
-		<div>
-			<h1>Twitter Friends</h1>
-			<SearchBox />
-			<CardList contacts={contacts}/>
-		</div>
-	)
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			contacts: contacts,
+			searchfield: ''
+		};
+	}
+	render() {
+		return (
+			<div className='tc'>
+				<h1>Twitter Friends</h1>
+				<SearchBox />
+				<CardList contacts={contacts}/>
+			</div>
+		)
+	}
 }
 
 export default App;
